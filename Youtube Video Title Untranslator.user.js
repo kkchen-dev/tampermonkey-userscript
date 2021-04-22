099d07c591451e074f7f6069468cc8b7056f816d
Kevin Chen <kevin.chen@neutral.land>
2021-04-21 22:18:55 -0700

    Refactor for readability

--- Youtube Video Title Untranslator.user.js
+++ Youtube Video Title Untranslator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Video Title Untranslator
 // @namespace    https://github.com/kkchen-dev/tampermonkey-userscript
-// @version      0.3
+// @version      0.4
 // @description  Translate the auto-translated Youtube video titles back to original languages
 // @author       Kevin Chen
 // @match        https://*.youtube.com/*
@@ -14,30 +14,31 @@
     function setTitle() {
         let originalTitleContainers = document.getElementsByClassName('ytp-title-text');
         let translatedTitleContainers = document.getElementsByClassName('title ytd-video-primary-info-renderer');
-        if (elementExists(originalTitleContainers)
-                && elementExists(translatedTitleContainers)
-                && elementExists(translatedTitleContainers[0].getElementsByTagName('yt-formatted-string'))) {
-            let originalTitle = originalTitleContainers[0].innerText;
-            let translatedElement = translatedTitleContainers[0].getElementsByTagName('yt-formatted-string')[0];
-            let translatedText = translatedElement.innerText;
 
-            if (elementExists(translatedElement.getElementsByTagName('span')) || originalTitle !== persistedTab) {
-                if (elementExists(translatedElement.getElementsByTagName('span'))) {
-                    translatedText = translatedElement.getElementsByTagName('span')[0].innerText;
-                }
-                if (originalTitle) {
-                    if (translatedText && translatedText !== originalTitle) {
-                        translatedElement.innerText = `${originalTitle} (${translatedText})`;
-                    }
-                    else {
-                        translatedElement.innerText = originalTitle;
-                    }
-                }
-                persistedTab = originalTitle;
+        if (!originalTitleContainers?.[0] || !translatedTitleContainers?.[0]?.getElementsByTagName('yt-formatted-string')?.[0]) {
+            return
+        }
+
+        let translatedElement = translatedTitleContainers[0].getElementsByTagName('yt-formatted-string')[0];
+        let originalTitle = originalTitleContainers[0].innerText;
+        if (!translatedElement.getElementsByTagName('span')?.[0] && originalTitle === persistedTab) {
+            return
+        }
+
+        let translatedText = translatedElement.innerText;
+        if (translatedElement.getElementsByTagName('span')?.[0]) {
+            translatedText = translatedElement.getElementsByTagName('span')[0].innerText;
+        }
+
+        if (originalTitle) {
+            if (translatedText && translatedText !== originalTitle) {
+                translatedElement.innerText = `${originalTitle} (${translatedText})`;
+            }
+            else {
+                translatedElement.innerText = originalTitle;
             }
         }
-    }
-    function elementExists(element) {
-        return !!element && !!element[0]
+
+        persistedTab = originalTitle;
     }
 })();
