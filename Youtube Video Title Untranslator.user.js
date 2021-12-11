4e3f703b4014ae099c6889ca7387401d247f888d
Kevin Chen <kevink.chen@aveva.com>
2021-12-10 20:11:35 -0800

    Improve original text injection

--- Youtube Video Title Untranslator.user.js
+++ Youtube Video Title Untranslator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Video Title Untranslator
 // @namespace    https://github.com/kkchen-dev/tampermonkey-userscript
-// @version      0.5
+// @version      0.6
 // @description  Translate the auto-translated Youtube video titles back to original languages
 // @author       Kevin Chen
 // @match        https://*.youtube.com/*
@@ -15,12 +15,20 @@
     function setTitle() {
         let originalTitle = document.getElementsByClassName('ytp-title-text')?.[0]?.innerText;
         let translatedTitle = JSON.parse(document.getElementById('scriptTag')?.innerText ?? "{}")?.name;
-        let displayedElement = document.getElementsByClassName('title ytd-video-primary-info-renderer')?.[0]?.getElementsByTagName('yt-formatted-string')?.[0];
+        let displayedElementContainer = document.getElementsByClassName('title ytd-video-primary-info-renderer')?.[0];
+        let displayedElement = displayedElementContainer?.getElementsByTagName('yt-formatted-string')?.[0];
         let currentId = document.getElementsByTagName('ytd-watch-flexy')?.[0]?.getAttribute('video-id');
 
         if (persistedId !== currentId && displayedElement && originalTitle && translatedTitle) {
             persistedId = currentId;
-            displayedElement.innerText = (originalTitle === translatedTitle) ? originalTitle : `${originalTitle} (${translatedTitle})`;
+            if (originalTitle !== translatedTitle)
+            {
+                let node = document.createElement("yt-formatted-string");
+                let text = document.createTextNode(`${originalTitle} || 🇹🇱: `);
+
+                displayedElementContainer.insertBefore(node, displayedElement);
+                node.appendChild(text);
+            }
         }
     }
 })();
